8968c35a5d0583ab91432b283ba8608f18af76ca
fahim.hadi <fahimhadimaula@gmail.com>
2024-12-05 20:29:18 +0700

    feat: generate random FOX generating random leterr of FOX. Fully random. There's no restriction on how many FOX can generated

--- client/src/pages/FindFox.jsx
+++ client/src/pages/FindFox.jsx
@@ -14,11 +14,19 @@ const [gridSize, setGridSize] = useState(3);
     setGridSize(gridSize + 1);
   };
 
+  const generateRandomFOX = (size) => {
+    const foxLetters = ['F', 'O', 'X'];
+    const gridBoard = Array.from({ length: size * size}, () => foxLetters[Math.floor(Math.random() * foxLetters.length)]);
+    return gridBoard;
+  };
+
+  const grid = generateRandomFOX(gridSize);
+
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="relative flex items-center justify-center h-screen">
       <div style={boardStyle}>
-        {Array.from({ length: gridSize * gridSize }).map((_, index) => (
-          <div key={index} className="w-[50px] border-[1px] border-white aspect-square"></div>
+        {grid.map((foxLetter, index) => (
+          <div key={index} className='w-[50px] border-[1px] border-white aspect-square flex items-center justify-center text-white'>{foxLetter}</div>
         ))}
       </div>
       <button onClick={handleButtonClick} className='absolute px-4 py-2 mt-4 text-white bg-blue-500 rounded bottom-4'>
