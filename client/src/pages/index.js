47fea2f0608cc42ee12a9e655c1f683edb675296
fahim.hadi <fahimhadimaula@gmail.com>
2024-12-10 02:55:14 +0700

    feat: version 2 of generating grid same as before

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,9 +1,11 @@
 import Home from "./Home";
 import Register from "./Register";
 import Login from "./Login";
+import FindFox from "./FindFox";
 
 export {
   Home,
   Register,
   Login,
+  FindFox,
 }
